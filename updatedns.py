35c3e2f8ecd03d91b5867a686a9a5d179aba4f76
birkoss <admin@birkoss.com>
2016-03-09 19:19:18 -0500

    Dynamic arguments + fetch IP if not specified

--- updatedns.py
+++ updatedns.py
@@ -1,6 +1,12 @@
 import base64
+import argparse
 from urllib.request import urlopen,Request
 import xml.etree.ElementTree as etree
+
+def fetch_external_ip(type):
+    url = 'http://' + ("ipv6" if type == "AAAA" else "ipv4") + '.myexternalip.com/raw'
+    ip = urlopen(url).read().decode('utf-8')[:-1]
+    return ip
 
 if __name__ == "__main__":
     try:
@@ -9,14 +15,25 @@ if __name__ == "__main__":
         print("Error: config.py NOT found")
         exit()
 
+    # Show all arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ttl', default='300', help='Time To Live')
+    parser.add_argument('--type', default='AAAA', help='Type of record: A for IPV4 or AAAA for IPV6')
+    parser.add_argument('--ip', help='The IPV4/IPV6 address (if known)')
+    parser.add_argument('--name', help='Your record name, ie: ipv6.domain.com', required=True)
+    parser.add_argument('--domain', help='The domain name containing the record name', required=True)
+    args = parser.parse_args()
+
     # Generate a auth_string to connect to cPanel
     auth_string = 'Basic ' + base64.b64encode((CONFIG['username']+':'+CONFIG['password']).encode()).decode("utf-8")
 
-    domain = "birkoss.com"
-    record = "ipv6.birkoss.com."
-    ip = "11:222:33:44:55:66:77:88"
-    type = "AAAA"
-    ttl = "300"
+    domain = args.domain
+    record = args.name
+    if not record.endswith('.'):
+        record += "."
+    type = ("AAAA" if args.type.upper() == "AAAA" else "A")
+    ip = args.ip if args.ip != None else fetch_external_ip(type)
+    ttl = args.ttl
 
     # Fetch existing DNS records
     q = Request(CONFIG['url'] + '/xml-api/cpanel?cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=fetchzone&cpanel_xmlapi_apiversion=2&domain=' + domain)
